4d411a2459e3e728a676377f0b90c0eae9569e55
Jhonatan <jhonatan@Jhonatan-2.local>
2026-01-30 22:27:59 -0300

    Initial frontend setup with Vite, React, TypeScript and Material UI

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="sm" sx={{ mt: 8 }}>
+      <Typography variant="h4" gutterBottom>
+        SIGLA – Frontend
+      </Typography>
+
+      <Button variant="contained">Teste Material UI</Button>
+    </Container>
+  );
+}
+
+export default App;
